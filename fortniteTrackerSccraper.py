ee9edb59a367d9591a41e85c6f4357574f2a492a
RogueMew <48804205+RogueMew@users.noreply.github.com>
2024-11-08 22:06:20 -0800

    Change to Logic for Fortnite Tracker Scraper

--- fortniteTrackerSccraper.py
+++ fortniteTrackerSccraper.py
@@ -1,9 +1,14 @@
 import time
 import random
+import pandas
+
+
+from tqdm import tqdm
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import pandas
-from tqdm import tqdm
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = webdriver.EdgeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -20,23 +25,35 @@ def scrapeRank(jsonData: dict, fileName: str)  -> None:
     time.sleep(random.randint(15, 20))
   
   df = pandas.DataFrame(columns=["Username", "Ranked BR", "Ranked ZB", "Ranked Reload BR", "Ranked Reload ZB"])
-  for school in tqdm(list(jsonData.keys()),desc="Scraping Ranks             ", bar_format="{l_bar}{bar:30}{r_bar}" ,total=len(list(jsonData.keys()))):
+  #for school in tqdm(list(jsonData.keys()),desc="Scraping Ranks             ", bar_format="{l_bar}{bar:30}{r_bar}" ,total=len(list(jsonData.keys()))):
+  for school in list(jsonData.keys()):
       for user in jsonData[school]:
-          if user['game_network_username'] is None:
-              break
+        if user['game_network_username'] is None:
+            break
           
-          driver = webdriver.Edge(options)
-          driver.get(f"https://fortnitetracker.com/profile/search?q={user['game_network_username'].replace(' ', '%20')}")    
+        driver = webdriver.Edge(options)
+        driver.get(f"https://fortnitetracker.com/profile/search?q={user['game_network_username'].replace(' ', '%20')}")    
+        
           
-          try:
-            driver.find_element(By.CSS_SELECTOR, '.trn-card--error')
-            driver.close()
-            break        
-          except:
-            text = removeUneeded(driver.find_element(By.CLASS_NAME, 'profile-current-ranks__content').text.split("\n"))
-            text.insert(0, user['game_network_username'])
-            df.loc[len(df.index)] = text
-            #print(df.loc[len(df.index)-1])
-            randomSleep()
-            driver.close()
+        try:
+          error_card = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.trn-card--error')))
+          df.loc[len(df.index)] = [user['game_network_username'], "Username404","Username404","Username404","Username404"]
+          driver.close()
+          break
+        except:
+          pass
+          
+        try:
+          rankText = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.profile-current-ranks__content')))
+          rankText = removeUneeded(rankText.text.split('\n'))
+          rankText.insert(0, user['game_network_username'])
+          df.loc[len(df.index)] = rankText
+          driver.close()
+          print(df.loc[len(df.index) - 1])
+          randomSleep()
+        except:
+          driver.close()
+          df.loc[len(df.index)] = [user['game_network_username'], "data404","data404","data404","data404"]
+          break
+            
   df.to_csv(f'{fileName}.csv', sep="," ,encoding='utf-8', index=False, header=True)
